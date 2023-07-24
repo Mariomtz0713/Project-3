eb38ba7b03da4af5ee2cda083b05e1274db98d8c
Mario Martinez <mariomtz2023@gmail.com>
2023-07-24 02:31:37 -0500

    Touched up analysis and created map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,9 +1,41 @@
+// Creating Map
+let myMap = L.map('map', {
+    center: [38.7339, -27.0647],
+    zoom: 2.5
+});
 
-d3.json('Resources/DataFiles/final_data.json').then((webData) => {
+// Adding tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-    console.log(webData); // Whole set
+// Executing json data
+d3.json('Resources/DataFiles/final_data.json').then((data) => {
 
+    console.log(data); 
+   
+    for (let i = 0; i < Object.keys(data.coordinates).length; i++) {
+                        // Since coordinates section is an object 
+                        // not an array, must use this method for .length
 
+        let latitude = data.coordinates[i][0];
+        let longtitude = data.coordinates[i][1];
+
+        // Displaying marker
+        let marker = L.marker([latitude, longtitude]).addTo(myMap);
+
+        // Displaying marker information
+        marker.bindPopup(
+        `City: ${data.city[i]} <br>
+        Airline: ${data.airline_name[i]} <br>
+        Price (USD): $${data.price[i]} <br>
+        Number of layovers: ${data.transfers[i]}`
+        ).addTo(myMap);
+
+        
+    }
+      
+    
     
 });
 
@@ -13,3 +45,6 @@ d3.json('Resources/DataFiles/final_data.json').then((webData) => {
 // bar chart - popular airline routes
 // line graph - tickets each day of the month, drop down menu for airlines
 // bubble chart on map?- destinations, bigger the bubble show popularity
+    // Create df only using city counter?
+    // Somehow seperate markers or use coordinates only df?
+    // Inlcude numer of flights? Avg price?
